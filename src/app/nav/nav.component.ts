404610926c96ebde067eeb0d4ee23a2e69e6233c
Bernhard Thüsch <bernhard.thuesch@prosiebensat1digital.de>
2021-01-01 22:41:25 +0100

    first commit

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+  showFiller = false;
+  constructor() { }
+
+}
